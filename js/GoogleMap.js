588dfc2899c7807f9e4c515d0a6e42961120c669
Viranda <viranda@Virandas-iMac.true.true>
2019-01-14 17:02:20 +0700

    sd

---
+++ js/GoogleMap.js
@@ -0,0 +1,47 @@
+$(document).ready(function (){
+		
+	'use strict';
+
+	/* Google Map
+	-----------------------------------------------------*/
+	
+	function mapInitialize() {
+
+	  // Change it to your location - use site like http://itouchmap.com/latlong.html
+	  var yourLatitude = 13.83232;
+	  var yourLongitude = 100.542728;
+		
+	  var dark = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}];
+	  var grey = [{"featureType":"all","elementType":"all","stylers":[{"saturation":-100},{"gamma":0.5}]}];
+	  var light =  [{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"stylers":[{"hue":"#00aaff"},{"saturation":-100},{"gamma":2.15},{"lightness":12}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"visibility":"on"},{"lightness":24}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":57}]}];
+	  var standard = [{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}];
+
+	  var pickedStyle = $('#google-map').data('style');	  
+	  var myOptions = {
+		zoom: 14,
+		center: new google.maps.LatLng(yourLatitude,yourLongitude-0.01),
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		mapTypeControl: false,
+		panControl: false,
+		zoomControl: true,
+		scaleControl: false,
+		streetViewControl: false,
+		scrollwheel: false,
+		styles: eval(pickedStyle)
+	  };
+
+	  var map = new google.maps.Map(document.getElementById('google-map'), myOptions);
+	  
+	  var image = 'images/my-location.png';
+	  var myLatLng = new google.maps.LatLng(yourLatitude,yourLongitude);
+	  var myLocation = new google.maps.Marker({
+		  position: myLatLng,
+		  map: map,
+		  icon: image
+	  });
+	
+	}
+	
+	google.maps.event.addDomListener(window, 'load', mapInitialize);
+	
+});
---
+++ php/contact-form.php
@@ -0,0 +1,20 @@
+<?php
+
+	$name = trim($_POST['name']);
+	$email = trim($_POST['email']);
+	$subject = trim($_POST['subject']);
+	$message = trim($_POST['message']);
+	
+	$emailTo = 'viranda.soonthornpakdee@gmail.com'; //Put your own email address here
+	if (empty($subject)) {
+	    $subject = 'Message from your website.';
+	}
+	$body = "Name: $name \n\nEmail: $email \n\nMessage:\n$message";
+	$headers = 'From: '.$email."\r\n" .
+        'Reply-To: '.$email."\r\n";
+
+	mail($emailTo, $subject, $body, $headers);
+	$emailSent = true;
+	echo ('SEND');
+	
+?>
